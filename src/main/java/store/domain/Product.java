3cb7d65a30998ad17aa6b1b98083769266a1fedc
hyuntae99 <hyuntae9912@naver.com>
2024-11-09 11:11:46 +0900

    feat: Product 클래스 구현

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,38 @@
+package store.domain;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private Promotion promotion;
+
+    public Product(String name, int price, int quantity, Promotion promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public void reduceQuantity(int amount) {
+        if (amount > quantity) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
+        quantity -= amount;
+    }
+}
