a6401ca01ed156ce2a8f5133a3fdcc88c19d7df4
hyuntae99 <hyuntae9912@naver.com>
2024-11-09 12:04:05 +0900

    feat: BuyResult 클래스 구현

---
+++ src/main/java/store/domain/BuyResult.java
@@ -0,0 +1,57 @@
+package store.domain;
+
+import java.util.List;
+import store.domain.Product;
+
+public class BuyResult {
+    private int cost;
+    private int freeQuantity;
+    private int nonPromotionAmount;
+    private List<Product> boughtProducts;
+    private List<Product> freeProducts;
+    private List<Product> modifiedProducts;
+    private int processedQuantity;
+
+    public BuyResult(int cost, int freeQuantity, int nonPromotionAmount, List<Product> boughtProducts, List<Product> freeProducts, List<Product> modifiedProducts) {
+        this.cost = cost;
+        this.freeQuantity = freeQuantity;
+        this.nonPromotionAmount = nonPromotionAmount;
+        this.boughtProducts = boughtProducts;
+        this.freeProducts = freeProducts;
+        this.modifiedProducts = modifiedProducts;
+        this.processedQuantity = 0;
+    }
+
+    public BuyResult(int cost, int freeQuantity, int nonPromotionAmount, List<Product> boughtProducts, List<Product> freeProducts, List<Product> modifiedProducts, int processedQuantity) {
+        this(cost, freeQuantity, nonPromotionAmount, boughtProducts, freeProducts, modifiedProducts);
+        this.processedQuantity = processedQuantity;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getFreeQuantity() {
+        return freeQuantity;
+    }
+
+    public int getNonPromotionAmount() {
+        return nonPromotionAmount;
+    }
+
+    public List<Product> getBoughtProducts() {
+        return boughtProducts;
+    }
+
+    public List<Product> getFreeProducts() {
+        return freeProducts;
+    }
+
+    public List<Product> getModifiedProducts() {
+        return modifiedProducts;
+    }
+
+    public int getProcessedQuantity() {
+        return processedQuantity;
+    }
+}
