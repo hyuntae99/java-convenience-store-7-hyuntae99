40388f7c59656be16140f2de8dad0dd6ea13f9fa
hyuntae99 <hyuntae9912@naver.com>
2024-11-09 11:11:52 +0900

    feat: Promotion 클래스 구현

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,37 @@
+package store.domain;
+
+import java.time.LocalDate;
+import camp.nextstep.edu.missionutils.DateTimes;
+
+public class Promotion {
+    private String name;
+    private int buy;
+    private int get;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public boolean isActive() {
+        LocalDate today = LocalDate.from(DateTimes.now());
+        return (today.isEqual(startDate) || today.isAfter(startDate)) && (today.isEqual(endDate) || today.isBefore(endDate));
+    }
+}
